5b36b6624fe34799b6e739889134145b1ec756e8
Henrique Vieira <henrique@mvstudio.be>
2022-09-15 09:06:43 +0200

    experimentation with filters

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,71 @@
-import { Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import supabase from '_supabase';
 
-import Col from '_comps/Layout/Col';
-import Container from '_comps/Layout/Container';
+export default function Dashboard({ data, error, dataKeys }) {
+	const [filterData, setFilterData] = useState(dataKeys);
+	const [filteredResult, setFilteredResult] = useState(data);
 
-export default function Dashboard() {
+	useEffect(() => {
+		setFilteredResult(
+			filteredResult.filter((entry) => {
+				for (const key in filterData) {
+					if (!entry[key].toString().toLowerCase().includes(filterData[key].toString().toLowerCase())) {
+						return false;
+					}
+				}
+
+				return true;
+			})
+		);
+	}, [filterData]);
+
 	return (
-		<Container>
-			<Col>
-				<Text>Dashboard page</Text>
-			</Col>
-		</Container>
+		<>
+			<h1>Test</h1>
+			<p>Fist Name</p>
+			<input
+				type="text"
+				onChange={(e) => {
+					setFilterData({ ...filterData, first_name: e.target.value });
+				}}
+			/>
+			<p>Last Name</p>
+			<input
+				type="text"
+				onChange={(e) => {
+					setFilterData({ ...filterData, last_name: e.target.value });
+				}}
+			/>
+			<hr />
+			<hr />
+			{filteredResult &&
+				filteredResult.map((entry) => (
+					<div key={entry.id}>
+						<ul>
+							<li>ID: {entry.id}</li>
+							<li>FIRST_NAME: {entry.first_name}</li>
+							<li>LAST NAME: {entry.last_name}</li>
+						</ul>
+						<hr />
+					</div>
+				))}
+		</>
 	);
 }
 
 export async function getServerSideProps() {
+	const { data, error } = await supabase.from('registrations').select();
+	let dataKeys = {};
+	if (data) {
+		for (const key in data[0]) {
+			dataKeys[key] = '';
+		}
+	}
 	return {
-		props: {},
+		props: {
+			data,
+			error,
+			dataKeys,
+		},
 	};
 }
